cb680ca9ae7f1f6c07f9bc4d83aaa795aef596c9
vineeth-nyllabs <vineeth@iOS-Vineeth.local>
2017-02-19 04:55:16 -0500

    adding components

--- HackNYU/services/DataService.js
+++ HackNYU/services/DataService.js
@@ -6,6 +6,44 @@ module.exports = function(app) {
 
     app.post("/api/newFormula",newEquasion);
     app.get("/api/latestFormula",getLatest);
+    app.get("/api/x1",getx1);
+    app.get("/api/y1",gety1);
+    app.get("/api/z1",getz1);
+    app.get("/api/x2",getx2);
+    app.get("/api/y2",gety2);
+    app.get("/api/z2",getz2);
+    app.get("/api/action",gettype);
+
+    function gettype(req,res) {
+        res.send("distance");
+    }
+
+    function getx1(req,res) {
+        res.send("1");
+    }
+
+    function gety1(req,res) {
+        res.send("2");
+    }
+
+    function getz1(req,res) {
+        res.send("3");
+    }
+
+
+    function getx2(req,res) {
+        res.send("3");
+    }
+
+    function gety2(req,res) {
+        res.send("2");
+    }
+    function getz2(req,res) {
+        res.send("1");
+    }
+
+
+
     app.get("/",home);
 
     function getLatest(req,res) {
